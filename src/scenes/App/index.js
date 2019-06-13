1372f45bb32a408783e5b592690b102dc3b04108
IvanovSP <ivanovsp88@gmail.com>
2019-06-13 15:48:23 +0300

    landing done

--- src/scenes/App/index.js
+++ src/scenes/App/index.js
@@ -48,6 +48,35 @@ const GlobalStyle = createGlobalStyle`
     height: 100%;
     flex-direction: column;
   }
+  
+  .x-large {
+    font-size: 4rem;
+    line-height: 1.2;
+    margin-bottom: 1rem;
+  }
+  
+  .lead {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+  }  
+  
+  .btn {
+    display: inline-block;
+    background: var(--light-color);
+    color: #333;
+    padding: 0.4rem 1.3rem;
+    font-size: 1rem;
+    border: none;
+    cursor: pointer;
+    margin-right: 0.5rem;
+    transition: opacity 0.2s ease-in;
+    outline: none;
+  }
+  
+  .btn-primary, .bg-primary, .badge-primary, .alert-primary {
+    background: var(--primary-color);
+    color: #fff;
+  }
 `;
 
 const App = () => (
--- src/scenes/Landing/index.js
+++ src/scenes/Landing/index.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import showcase from '@/assets/img/showcase.jpg';
 
 const Wrapper = styled.section`
   flex: 1;
   display: flex;
+  background-image: url(${showcase});
+  color: white;
+  background-size: cover;
+  background-position: center;
+`;
+
+const DarkOverlay = styled.section`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-image: url(${showcase});
+  background-color: rgba(0, 0, 0, 0.7);
 `;
 
 
 export default () => (
   <Wrapper>
-    <h1>Landing</h1>
+    <DarkOverlay>
+      <h1 className="x-large">Developer Connector</h1>
+      <p className="lead">
+        Create a developer profile/portfolio, share posts and get help from
+        other developers
+      </p>
+      <div className="buttons">
+        <Link to="/register" className="btn btn-primary">Sign Up</Link>
+        <Link to="/login" className="btn btn-light">Login</Link>
+      </div>
+    </DarkOverlay>
   </Wrapper>
 );
